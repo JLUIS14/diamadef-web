88215a17d861451e70ab56bcef7753089acbef61
Jorge Esteban <jlea14@gmail.com>
2025-09-06 19:23:19 -0500

    Se actualizo el HeroSection se actualizo el index.cssse agrego imagenes

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 
 const images = [
-  "/images/img2.jpg",
+  "/images/img14.jpg",
   "/images/img4.jpg",
-  "/images/img14.jpg", // ✅ agrega las imágenes que quieras
+  "/images/img13.jpg",
+   "/images/img2.jpg"// 
 ];
 
 export const HeroSection = () => {
@@ -17,8 +18,6 @@ export const HeroSection = () => {
     return () => clearInterval(interval);
   }, []);
 
-  // Funciones de navegación manual
-  const goToSlide = (index: number) => setCurrentImage(index);
   const prevSlide = () =>
     setCurrentImage((prev) => (prev - 1 + images.length) % images.length);
   const nextSlide = () =>
@@ -36,22 +35,20 @@ export const HeroSection = () => {
 
       {/* Contenido */}
       <div className="relative z-10">
-        <h1 className="text-4xl md:text-5xl font-extrabold text-white drop-shadow mb-4">
+        <h1 className="text-4xl md:text-5xl font-extrabold text-yellow-400 drop-shadow mb-4 opacity-0 animate-fadeInUp">
           Bienvenido a Inversiones Diamadef
         </h1>
-        <p className="text-lg md:text-2xl text-white mb-8 max-w-2xl drop-shadow">
+        <p className="text-lg md:text-2xl text-yellow-300 mb-8 max-w-2xl mx-auto text-center drop-shadow animate-fadeInUp-delay1 bg-black bg-opacity-10 p-2 rounded">
           Diseñamos productos únicos en madera con responsabilidad ambiental y
           calidad garantizada.
         </p>
         <a
           href="#productos"
-          className="inline-block px-8 py-3 bg-white text-green-800 font-semibold rounded-full shadow hover:bg-yellow-300 transition"
+          className="inline-block px-8 py-3 bg-white text-green-800 font-semibold rounded-full shadow hover:bg-yellow-300 transition opacity-0 animate-fadeInUp-delay2"
         >
           Explora nuestros productos
         </a>
       </div>
-
-      
 
       {/* ⬅️ Flecha izquierda */}
       <button
